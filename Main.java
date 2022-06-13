923074b1dd72c17916b1ba4e42fd2ea99e67c6a6
port <m1m22a25@oit.ac.jp>
2022-06-13 14:07:01 +0900

    add player

--- Main.java
+++ Main.java
@@ -2,6 +2,9 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("SEMONTER!");
     Monster monster = new Monster();
+    Player player = new Player();
+    player.drawMonsters();
+    player.showDeck();
     System.out.println(monster.toString());
   }
 }
