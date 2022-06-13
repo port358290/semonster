bb0aec21ab4e50e951c6da8618e0c7d7df006c54
port <m1m22a25@oit.ac.jp>
2022-06-13 13:57:29 +0900

    solve conflict

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello SEMonster");
+    System.out.println("SEMONTER!");
+    Monster monster = new Monster();
+    System.out.println(monster.toString());
   }
 }
